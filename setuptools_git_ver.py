905efaf28bea18bb65d17640f4e318936682a4ea
Camas <camas@hotmail.co.uk>
2019-10-25 17:11:17 +0100

    Remove default. Add commits since

--- setuptools_git_ver.py
+++ setuptools_git_ver.py
@@ -2,8 +2,8 @@ from typing import List
 import subprocess
 
 DEFTAULT_TEMPLATE: str = "{tag}"
-DEFAULT_DEV_TEMPLATE: str = "{tag}.dev+git.{sha}"
-DEFAULT_DIRTY_TEMPLATE: str = "{tag}.dev+git.{sha}.dirty"
+DEFAULT_DEV_TEMPLATE: str = "{tag}.dev{ccount}+git.{sha}"
+DEFAULT_DIRTY_TEMPLATE: str = "{tag}.dev{ccount}+git.{sha}.dirty"
 
 
 def _exec(cmd: List[str]) -> List[str]:
@@ -21,7 +21,7 @@ def _get_tag():
 
 
 def _get_sha(name):
-    sha = _exec(f"git rev-list -n 1 '{name}'".split(' '))
+    sha = _exec([*f"git rev-list -n 1".split(' '), name])
     if len(sha) == 0 or len(sha[0]) == 0:
         return None
     return sha[0]
@@ -30,33 +30,33 @@ def _get_sha(name):
 def _is_dirty():
     res = _exec("git status --short".split(' '))
     if len(res) == 0 or len(res[0]) == 0:
-        return True
-    return False
+        return False
+    return True
+
+
+def _count_since(name):
+    res = _exec([*"git rev-list --count HEAD".split(' '), f"^{name}"])
+    return int(res[0])
 
 
 def version_from_git(template: str = DEFTAULT_TEMPLATE,
                      dev_template: str = DEFAULT_DEV_TEMPLATE,
                      dirty_template: str = DEFAULT_DIRTY_TEMPLATE,
-                     default_ver: str = None,
                      ) -> None:
     tag = _get_tag()
     dirty = _is_dirty()
     if tag is None:
-        if default_ver is None:
-            raise Exception(
-                "Couldn't find tag to use and no default was specified")
-        tag = default_ver
-        tag_sha = _get_sha('HEAD')
-        t = dirty_template if dirty else dev_template
+        raise Exception("Couldn't find tag to use.")
+
+    tag_sha = _get_sha(tag)
+    head_sha = _get_sha('HEAD')
+    ccount = _count_since(tag)
+
+    if dirty:
+        t = dirty_template
+    elif head_sha != tag_sha:
+        t = dev_template
     else:
-        tag_sha = _get_sha(tag)
-        head_sha = _get_sha('HEAD')
+        t = template
 
-        if dirty:
-            t = dirty_template
-        elif head_sha != tag_sha:
-            t = dev_template
-        else:
-            t = template
-
-    return t.format(sha=tag_sha[:8], tag=tag)
+    return t.format(sha=tag_sha[:8], tag=tag, ccount=ccount)
