ad23f99e5a0b23927e51df8b8b98fc85c17eda32
Camas <camas@hotmail.co.uk>
2019-10-25 18:54:42 +0100

    Fix python version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     py_modules=['setuptools_git_ver'],
     install_requires=['setuptools'],
     entry_points={
