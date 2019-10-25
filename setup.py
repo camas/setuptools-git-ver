a6692e2af73b5abfb10d4a763517cc78445698de
Camas <camas@hotmail.co.uk>
2019-10-25 19:15:39 +0100

    python > 3.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     py_modules=['setuptools_git_ver'],
     install_requires=['setuptools'],
     entry_points={
